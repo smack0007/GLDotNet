554edef47673da9cf22754a56273cd39688bc451
smack0007 <zachary.snow@gmail.com>
2018-04-21 23:42:03 +0200

    Added HelloTextures sample.

--- samples/HelloTextures/HelloTexturesSample.cs
+++ samples/HelloTextures/HelloTexturesSample.cs
@@ -1,10 +1,7 @@
-﻿///////////////////////
-// This example is modified version of the example found at
-// https://github.com/capnramses/antons_opengl_tutorials_book/blob/master/03_vertex_buffer_objects/main.cpp
-///////////////////////
-
-using System;
+﻿using System;
+using System.IO;
 using GLDotNet.Samples;
+using ImageDotNet;
 using static GLDotNet.GL;
 
 namespace HelloTriangle
@@ -17,33 +14,44 @@ namespace HelloTriangle
         private const string VertexShader = @"#version 400
 layout(location = 0) in vec3 vertPosition;
 layout(location = 1) in vec3 vertColor;
+layout(location = 2) in vec2 vertUV;
 
 out vec3 fragColor;
+out vec2 fragUV;
 
 void main()
 {
-	fragColor = vertColor;
 	gl_Position = vec4(vertPosition, 1.0);
+	fragColor = vertColor;
+    fragUV = vertUV;
 }";
 
         private const string FragmentShader = @"#version 400
 in vec3 fragColor;
+in vec2 fragUV;
+
 out vec4 outColor;
+
+uniform sampler2D fragTexture;
 
 void main()
 {
-	outColor = vec4(fragColor, 1.0);
+	outColor = texture(fragTexture, fragUV);
 }";
 
         private uint vertexArray;
+        private uint texture;
         private uint shaderProgram;
 
         public HelloTriangleSample()
         {
-            this.Title = "Hello Triangle";
+            this.Title = "Hello Textures";
+
+            var basePath = Path.GetDirectoryName(typeof(HelloTriangleSample).Assembly.Location);
 
             float[] points = new float[] { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
             float[] colors = new float[] { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+            float[] uv = new float[] { 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f };
 
             var pointsBuffer = glGenBuffer();
             glBindBuffer(GL_ARRAY_BUFFER, pointsBuffer);
@@ -53,14 +61,35 @@ void main()
             glBindBuffer(GL_ARRAY_BUFFER, colorsBuffer);
             glBufferData(GL_ARRAY_BUFFER, sizeof(float) * colors.Length, colors, GL_STATIC_DRAW);
 
+            var uvBuffer = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(float) * uv.Length, uv, GL_STATIC_DRAW);
+
             this.vertexArray = glGenVertexArray();
             glBindVertexArray(this.vertexArray);
             glBindBuffer(GL_ARRAY_BUFFER, pointsBuffer);
             glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, IntPtr.Zero);
             glBindBuffer(GL_ARRAY_BUFFER, colorsBuffer);
             glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, IntPtr.Zero);
+            glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+            glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, IntPtr.Zero);
             glEnableVertexAttribArray(0);
             glEnableVertexAttribArray(1);
+            glEnableVertexAttribArray(2);
+
+            this.texture = glGenTexture();
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, this.texture);
+                        
+            // Image is an RGBImage.
+            var image = Image.LoadTga(Path.Combine(basePath, "Texture.tga"));
+            using (var data = image.GetDataPointer())
+            {
+                glTexImage2D(GL_TEXTURE_2D, 0, (int)GL_RGB, image.Width, image.Height, 0, GL_RGB, GL_UNSIGNED_BYTE, data.Pointer);
+            }
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (int)GL_LINEAR);
 
             uint vs = GLUtility.CreateAndCompileShader(GL_VERTEX_SHADER, VertexShader);
             uint fs = GLUtility.CreateAndCompileShader(GL_FRAGMENT_SHADER, FragmentShader);
@@ -83,6 +112,7 @@ void main()
 
             glUseProgram(this.shaderProgram);
             glBindVertexArray(this.vertexArray);
+            glUniform1i(0, 0);
             glDrawArrays(GL_TRIANGLES, 0, 3);
         }
 
