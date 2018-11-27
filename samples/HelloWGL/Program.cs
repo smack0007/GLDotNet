ebdd1621aaec52d2d580a8576543405ddae411bb
smack0007 <zachary.snow@gmail.com>
2018-11-27 21:50:00 +0100

    Added HelloWGL sample.

---
+++ samples/HelloWGL/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static HelloWGL.Win32;
+using static GLDotNet.GL;
+using static GLDotNet.WGL;
+
+namespace HelloWGL
+{
+    class Program
+    {
+        private static readonly string WindowClass = "HelloWGLWindow";
+
+        public static void Main(string[] args)
+        {
+            var wndClass = new WNDCLASSEX();
+            wndClass.cbSize = (uint)Marshal.SizeOf(wndClass);
+            wndClass.hInstance = GetModuleHandle(null);
+            wndClass.lpfnWndProc = WndProc;
+            wndClass.lpszClassName = WindowClass;
+            wndClass.hCursor = LoadCursor(IntPtr.Zero, IDC_ARROW);
+
+            var @class = RegisterClassEx(ref wndClass);
+
+            var window = CreateWindowEx(
+                WS_EX_APPWINDOW,
+                WindowClass,
+                "Hello WGL",
+                WS_OVERLAPPEDWINDOW,
+                0,
+                0,
+                800,
+                600,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                IntPtr.Zero);
+
+            wglInit(window, 4, 0);
+            glInit(wglGetProcAddress, 4, 0);
+            glClearColor(1.0f, 0, 1.0f, 1.0f);
+
+            ShowWindow(window, SW_SHOWNORMAL);
+
+            bool running = true;
+
+            while (running)
+            {
+                if (PeekMessage(out MSG message, IntPtr.Zero, 0, 0, PM_REMOVE))
+                {
+                    switch (message.msg)
+                    {
+                        case WM_QUIT:
+                            running = false;
+                            break;
+                    }
+
+                    TranslateMessage(ref message);
+                    DispatchMessage(ref message);
+                }
+                else
+                {
+                    // TODO: Game loop
+                }
+            }
+        }
+
+        private static IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (msg)
+            {
+                case WM_DESTROY:
+                    PostQuitMessage(0);
+                    break;
+
+                case WM_PAINT:
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    wglSwapBuffers();
+                    break;
+            }
+
+            return DefWindowProc(hWnd, msg, wParam, lParam);
+        }
+    }
+}
---
+++ samples/HelloWGL/Win32.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWGL
+{
+    public static class Win32
+    {
+        public const int IDC_ARROW = 32512;
+
+        public const int PM_REMOVE = 0x0001;
+
+        public const int SW_SHOWNORMAL = 1;
+
+        public const int WM_DESTROY = 0x0002;
+        public const int WM_PAINT = 0x000F;
+        public const int WM_QUIT = 0x0012;
+
+        public const int WS_CAPTION = 0x00C00000;
+        public const int WS_MAXIMIZEBOX = 0x00010000;
+        public const int WS_MINIMIZEBOX = 0x00020000;
+        public const int WS_OVERLAPPED = 0x00000000;
+        public const int WS_OVERLAPPEDWINDOW = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+        public const int WS_SYSMENU = 0x00080000;
+        public const int WS_THICKFRAME = 0x00040000;
+
+        public const int WS_EX_APPWINDOW = 0x00040000;
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WNDCLASSEX
+        {
+            public uint cbSize;
+            public uint style;
+            [MarshalAs(UnmanagedType.FunctionPtr)]
+            public WndProc lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            public string lpszMenuName;
+            public string lpszClassName;
+            public IntPtr hIconSm;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
+        {
+            public IntPtr hWnd;
+            public uint msg;
+            public IntPtr wParam;
+            public IntPtr lParam;
+            public uint time;
+            public POINT pt;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Auto)]
+        public static extern IntPtr CreateWindowEx(
+            int exStyle,
+            string className,
+            string windowName,
+            int style,
+            int x,
+            int y,
+            int width,
+            int height,
+            IntPtr hWndParent,
+            IntPtr hMenu,
+            IntPtr hInstance,
+            IntPtr pvParam);
+
+        public delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        public static readonly WndProc DefaultWindowProc = DefWindowProc;
+
+        [DllImport("user32.dll", EntryPoint = "DefWindowProc")]
+        public static extern IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Auto)]
+        public static extern bool DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "DispatchMessage")]
+        public static extern IntPtr DispatchMessage([In] ref MSG msg);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetModuleHandle")]
+        public static extern IntPtr GetModuleHandle(string module);
+
+        [DllImport("gdi32.dll", EntryPoint = "GetStockObject")]
+        public static extern IntPtr GetStockObject(int fnObject);
+
+        [DllImport("user32.dll", EntryPoint = "LoadCursor")]
+        public static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+
+        [DllImport("user32.dll", EntryPoint = "PeekMessage")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PeekMessage(out MSG msg, IntPtr hWnd, uint messageFilterMin, uint messageFilterMax, uint flags);
+
+        [DllImport("user32.dll", EntryPoint = "PostQuitMessage")]
+        public static extern void PostQuitMessage(int nExitCode);
+
+        [DllImport("user32.dll", EntryPoint = "RegisterClassEx")]
+        [return: MarshalAs(UnmanagedType.U2)]
+        public static extern short RegisterClassEx([In] ref WNDCLASSEX lpwcx);
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", EntryPoint = "TranslateMessage")]
+        public static extern bool TranslateMessage([In] ref MSG msg);
+    }
+}
