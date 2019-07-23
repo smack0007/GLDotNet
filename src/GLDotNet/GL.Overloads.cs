715b61f8463707d411544ab3985506dc07dfbaf9
smack0007 <zachary.snow@gmail.com>
2019-07-23 09:38:16 +0200

    Added more overloads.

--- src/GLDotNet/GL.Overloads.cs
+++ src/GLDotNet/GL.Overloads.cs
@@ -5,8 +5,7 @@ namespace GLDotNet
 {
     public static unsafe partial class GL
     {
-        public static void glBufferData<T>(uint target, int size, IntPtr data, uint usage)
-            where T : unmanaged
+        public static void glBufferData(uint target, int size, IntPtr data, uint usage)
         {
             glBufferData(target, size, data.ToPointer(), usage);
         }
@@ -18,6 +17,26 @@ namespace GLDotNet
             {
                 glBufferData(target, size, dataPtr, usage);
             }
+        }
+
+        public static void glDeleteBuffer(uint buffer)
+        {
+            glDeleteBuffers(1, (uint*)buffer);
+        }
+
+        public static void glDeleteTexture(uint texture)
+        {
+            glDeleteVertexArrays(1, (uint*)texture);
+        }
+
+        public static void glDeleteVertexArray(uint vertexArray)
+        {
+            glDeleteVertexArrays(1, (uint*)vertexArray);
+        }
+
+        public static void glDrawElements(uint mode, int count, uint type, IntPtr indices)
+        {
+            glDrawElements(mode, count, type, indices.ToPointer());
         }
 
         public static uint glGenBuffer()
@@ -41,6 +60,14 @@ namespace GLDotNet
             return vertexArray;
         }
 
+        public static void glGetIntegerv(uint pname, ref int data)
+        {
+            fixed (int* dataPtr = &data)
+            {
+                glGetIntegerv(pname, dataPtr);
+            }
+        }
+
         public static int glGetProgrami(uint program, uint pname)
         {
             int param = 0;
@@ -58,6 +85,19 @@ namespace GLDotNet
         public static void glTexImage2D(uint target, int level, int internalformat, int width, int height, int border, uint format, uint type, IntPtr pixels)
         {
             glTexImage2D(target, level, internalformat, width, height, border, format, type, pixels.ToPointer());
+        }
+
+        public static void glUniformMatrix4fv(int location, int count, bool transpose, ref float value)
+        {
+            fixed (float* valuePtr = &value)
+            {
+                glUniformMatrix4fv(location, count, transpose, valuePtr);
+            }
+        }
+
+        public static void glVertexAttribPointer(uint index, int size, uint type, bool normalized, int stride, IntPtr pointer)
+        {
+            glVertexAttribPointer(index, size, type, normalized, stride, pointer.ToPointer());
         }
 
 #if !GLDOTNET_EXCLUDE_SYSTEM_MEMORY
